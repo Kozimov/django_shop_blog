bb61e9e68773b6783fde5ee3b623bfc81c3cbebf
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-03-26 16:12:54 +0500

    img create

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,9 @@
+from distutils.command.upload import upload
 from django.db import models
 
 class Post(models.Model):
     nomi = models.CharField(max_length=20)
+    rasm = models.ImageField(upload_to="rasmlar/")
 
     def __str__(self):
         return self.nomi
--- blog/settings.py
+++ blog/settings.py
@@ -114,6 +114,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
